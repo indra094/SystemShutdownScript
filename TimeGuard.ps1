9823e76d01e44d1c3ced0136f90160919d119ab1
INDRAJEET NANDY <ij.nandy10@gmail.com>
2026-07-10 08:13:21 -0700

    Update TimeGuard.ps1

--- TimeGuard.ps1
+++ TimeGuard.ps1
@@ -1,1 +1,31 @@
+while ($true) {
 
+    $now = Get-Date
+    $hour = $now.Hour
+    $minute = $now.Minute
+
+    # Reminder at 4:30 PM
+    if ($hour -eq 16 -and $minute -eq 30) {
+
+        Add-Type -AssemblyName PresentationFramework
+
+        [System.Windows.MessageBox]::Show(
+            "30 minutes remaining. Plan your next day for 4 hours before shutdown.",
+            "TimeGuard Reminder"
+        )
+
+    }
+
+
+    # Shutdown after 5 PM
+    if ($hour -ge 17) {
+
+        shutdown.exe /s /t 30 `
+        /c "Work window ended. Shutting down."
+
+        exit
+    }
+
+
+    Start-Sleep -Seconds 60
+}
